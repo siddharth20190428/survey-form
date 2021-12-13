2f4b91ff5eba7c4f2480a22e092250e03cd7baaa
Siddharth <siddharth201820@gmail.com>
2021-12-13 17:09:52 +0530

    reduced code and added comments

--- script.js
+++ script.js
@@ -1,74 +1,84 @@
 import { questions } from "./questions.js";
 
+function setAttrs(element, attrs) {
+  attrs.forEach((attr) => element.setAttribute(attr[0], attr[1]));
+}
+
+// form progress
 const progressCont = document.querySelector(".progress-cont");
+let progItems = document.querySelectorAll(".prog-item");
 
 for (let i = 0; i < questions.length + 1; i++) {
   let item = document.createElement("div");
   item.classList.add("prog-item");
   progressCont.appendChild(item);
 }
+
 let answers = [];
-let optionsForQues = [];
-
 let currQuestion = 0;
-let progItems = document.querySelectorAll(".prog-item");
 
+// question and options container
 const container = document.querySelector(".form-cont");
 const ques = document.createElement("div");
 let optionholder = document.createElement("div");
 
 let slider = document.createElement("INPUT");
+setAttrs(slider, [
+  ["class", "slider"],
+  ["type", "range"],
+  ["min", "1"],
+  ["max", "10"],
+]);
 
 const next = document.querySelector("#next-btn");
-
-slider.setAttribute("type", "range");
-slider.setAttribute("min", "1");
-slider.setAttribute("max", "10");
 
 const createques = () => {
   if (currQuestion < questions.length) {
     ques.classList.add("form-content");
     ques.innerText =
       [currQuestion + 1] + ". " + questions[currQuestion].question;
+    optionholder.classList.add("options");
 
     if (currQuestion < 3) {
-      optionholder.classList.add("options");
-
+      // for questions having radio buttons
       let options = questions[currQuestion].options;
 
       options.map((element, opId) => {
+        // creating input for option
         let option = document.createElement("input");
+        setAttrs(option, [
+          ["type", "radio"],
+          ["name", "option"],
+          ["value", element],
+          ["id", `option-${opId}`],
+        ]);
+
+        // creating label for option input
         let optionLabel = document.createElement("label");
-        option.setAttribute("type", "radio");
-        option.setAttribute("name", "option");
-        option.setAttribute("value", element);
-        option.setAttribute("id", `option-${opId}`);
         optionLabel.setAttribute("for", `option-${opId}`);
+        // adding text for option
         optionLabel.innerHTML = `<p class="label-para">${element}</p>`;
         optionLabel.appendChild(option);
         optionLabel.classList.add("option-label");
+
         optionholder.appendChild(optionLabel);
 
+        // the next button is disabled till the user selects any option
         if (options.length) {
           next.removeEventListener("click", onNextClick);
         }
         option.addEventListener("change", () =>
           next.addEventListener("click", onNextClick)
         );
-
-        optionsForQues.push(option);
       });
     } else {
-      optionholder.classList.add("options");
-      slider.classList.add("slider");
-
       optionholder.appendChild(slider);
-      let val = slider.value;
 
+      // getting the value which user selects in slider
+      let val = slider.value;
       let silderval = document.createElement("span");
       silderval.innerHTML = val;
       optionholder.appendChild(silderval);
-      optionsForQues.push(slider);
 
       slider.oninput = function () {
         silderval.innerHTML = this.value;
@@ -76,17 +86,21 @@ const createques = () => {
 
       answers.push(val);
     }
+    // pushing the question and options in the form container
     container.appendChild(ques);
     container.appendChild(optionholder);
   } else {
+    // end of the questions
     container.innerHTML = "THANK YOU FOR YOUR RESPONSE";
     next.remove();
   }
 };
 
 function onNextClick() {
+  // setting progress bar
   progItems[currQuestion].classList.add("dark-prog-item");
 
+  // reset the containers
   container.innerHTML = "";
   optionholder.innerHTML = "";
 
